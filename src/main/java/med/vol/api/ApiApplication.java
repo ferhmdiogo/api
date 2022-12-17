13eeac57efb356561977f91a76c1d2c3b9ff61d6
ferhmdiogo <ferhmdiogo@gmail.com>
2022-12-16 23:46:53 -0300

    Configurando o projeto

--- src/main/java/med/vol/api/ApiApplication.java
+++ src/main/java/med/vol/api/ApiApplication.java
@@ -3,7 +3,9 @@ package med.vol.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/med/vol/api/controller/HelloController.java
+++ src/main/java/med/vol/api/controller/HelloController.java
@@ -1,5 +1,6 @@
 package med.vol.api.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping
+
     public String olaMundo(){
 
         return "Olá Mundo";
