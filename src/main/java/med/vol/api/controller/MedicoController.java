eb12d69ae6e8f0a8f7b546ab42235339f8a8707b
ferhmdiogo <ferhmdiogo@gmail.com>
2023-01-30 23:13:06 -0300

    Configurando o projeto

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,18 +1,41 @@
 package med.vol.api.controller;
 
+import jakarta.validation.Valid;
+import med.vol.api.dto.DadosAtualizacaoMedico;
+import med.vol.api.dto.DadosListar;
 import med.vol.api.dto.MedicoDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.vol.api.model.MedicoModel;
+import med.vol.api.repositories.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastrar(@RequestBody MedicoDto medico){
-        System.out.println(medico);
+    @Transactional
+    public void cadastrar(@RequestBody @Valid MedicoDto dados){
+        repository.save(new MedicoModel(dados));
+
+    }
+
+    @GetMapping
+    public Page<DadosListar> listar(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListar::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
 
     }
 }
--- src/main/java/med/vol/api/model/MedicoModel.java
+++ src/main/java/med/vol/api/model/MedicoModel.java
@@ -1,4 +1,41 @@
 package med.vol.api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.vol.api.dto.Especialidade;
+import med.vol.api.dto.MedicoDto;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class MedicoModel {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+
+    private String telefone;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public MedicoModel(MedicoDto dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
